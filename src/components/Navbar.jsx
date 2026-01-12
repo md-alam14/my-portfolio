bad1f9306ee49a04dd3ecbd301b43284f8abd863
Md Nafish Alam <mdsheikh6234@gmail.com>
2026-01-12 10:45:44 +0530

    Update Navbar logo to show full name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     return (
         <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
             <div className="container nav-content">
-                <a href="#home" className="logo">NAFISH.</a>
+                <a href="#home" className="logo">MD NAFISH ALAM.</a>
                 <ul className="nav-links">
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
